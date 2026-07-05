6eb2f7d5616ed2a95ae5ef65223014c22c5625d2
miounet11 <9248293@gmail.com>
2026-07-06 01:08:29 +0800

    Fix Cowork UITest hang on svp: close dialog without DialogClosed wait
    Headless svp does not reliably emit DialogClosed after cancel CLICK.
    Skip execute_dialog_through_command's close waiter and dismiss via
    cancel + waitUntilAllIdlesDispatched instead.
    
    Change-Id: I7699ef91f4117a12c226450ee69614f8a024b7b9

--- cui/qa/uitest/cowork/test_cowork_dialog.py
+++ cui/qa/uitest/cowork/test_cowork_dialog.py
@@ -6,6 +6,8 @@
 
 import time
 
+from com.sun.star.awt import Toolkit
+
 from uitest.framework import UITestCase
 from uitest.uihelper.common import get_state_as_dict
 
@@ -13,8 +15,14 @@ from uitest.uihelper.common import get_state_as_dict
 class CoworkDialog(UITestCase):
 
     def _open_cowork_dialog(self):
+        # svp/headless: cancel via UITest does not always emit DialogClosed;
+        # close manually instead of execute_dialog_through_command's waiter.
         return self.ui_test.execute_dialog_through_command(
-            ".uno:CoworkTaskManager", close_button="cancel")
+            ".uno:CoworkTaskManager", close_button="")
+
+    def _close_cowork_dialog(self, xDialog):
+        xDialog.getChild("cancel").executeAction("CLICK", tuple())
+        Toolkit.create(self.xContext).waitUntilAllIdlesDispatched()
 
     def test_a_cowork_dialog_controls_smoke(self):
         with self.ui_test.create_doc_in_start_center("writer"), self._open_cowork_dialog() as xDialog:
@@ -22,6 +30,7 @@ class CoworkDialog(UITestCase):
             self.assertIsNotNone(xDialog.getChild("btn_accept_task"))
             self.assertIsNotNone(xDialog.getChild("task_list_view"))
             self.assertIsNotNone(xDialog.getChild("status_label"))
+            self._close_cowork_dialog(xDialog)
 
     def test_b_cowork_new_task_visible_in_list(self):
         sleep_s = self.ui_test.get_default_sleep()
@@ -36,6 +45,7 @@ class CoworkDialog(UITestCase):
                     break
                 time.sleep(sleep_s)
             self.assertNotEqual(get_state_as_dict(xList).get("Children", "0"), "0")
+            self._close_cowork_dialog(xDialog)
 
     def test_c_cowork_accept_task_enabled_after_review(self):
         sleep_s = self.ui_test.get_default_sleep()
@@ -53,6 +63,7 @@ class CoworkDialog(UITestCase):
             self.assertEqual(
                 get_state_as_dict(xAccept).get("Enabled", "false"), "true")
             xAccept.executeAction("CLICK", tuple())
+            self._close_cowork_dialog(xDialog)
 
 
 # vim: set shiftwidth=4 softtabstop=4 expandtab:
